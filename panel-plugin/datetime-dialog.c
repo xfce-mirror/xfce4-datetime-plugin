d49f8d0dfd6c3b14e971e0de1e1350ebba8b86de
Landry Breuil <landry@xfce.org>
2016-05-01 11:29:02 +0200

    GTK_DIALOG_NO_SEPARATOR was removed, and use "gtk-close"/"gtk-help" instead of GTK_STOCK_CLOSE/GTK_STOCK_HELP

--- panel-plugin/datetime-dialog.c
+++ panel-plugin/datetime-dialog.c
@@ -347,10 +347,10 @@ datetime_properties_dialog(XfcePanelPlugin *plugin, t_datetime * datetime)
   xfce_textdomain (GETTEXT_PACKAGE, LOCALEDIR, "UTF-8");
 
   dlg = xfce_titled_dialog_new_with_buttons(_("Datetime"),
-      NULL, /* or: GTK_WINDOW(gtk_widget_get_toplevel(GTK_WIDGET(plugin))), */
-      GTK_DIALOG_DESTROY_WITH_PARENT | GTK_DIALOG_NO_SEPARATOR,
-      GTK_STOCK_HELP, GTK_RESPONSE_HELP,
-      GTK_STOCK_CLOSE, GTK_RESPONSE_OK,
+      GTK_WINDOW(gtk_widget_get_toplevel(GTK_WIDGET(plugin))),
+      GTK_DIALOG_DESTROY_WITH_PARENT,
+      "gtk-help", GTK_RESPONSE_HELP,
+      "gtk-close", GTK_RESPONSE_OK,
       NULL);
 
   g_object_set_data(G_OBJECT(plugin), "dialog", dlg);
